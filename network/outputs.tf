90f34a345533e0918cede287aef10c288fd39b26
insian <patrasoham1512@gmail.com>
2025-05-02 11:33:08 +0530

    added outputs.tf

---
+++ network/outputs.tf
@@ -0,0 +1,12 @@
+# Inside modules/network/outputs.tf
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+}
+
+output "security_group_id" {
+  value = aws_security_group.ecs_sg.id
+}
